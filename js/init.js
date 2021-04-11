5c2112b48c39235e4f9168870e21718c12f41754
brennrosa22 <brennrosa22@gmail.com>
2021-04-10 21:09:18 -0500

    Version 1

---
+++ js/init.js
@@ -0,0 +1,13 @@
+// Initialize Firebase
+firebase.initializeApp(
+  {
+    apiKey: "AIzaSyCQGkkkUgpivrPbvdIIx8vLyke1kzoKySg",
+    authDomain: "controliot-f08d0.firebaseapp.com",
+    projectId: "controliot-f08d0",
+    storageBucket: "controliot-f08d0.appspot.com",
+    messagingSenderId: "591230748203",
+    appId: "1:591230748203:web:35021fa56da4bd1287cc6e",
+    measurementId: "G-KH43VNXKT6"
+}
+);
+firebase.analytics();
